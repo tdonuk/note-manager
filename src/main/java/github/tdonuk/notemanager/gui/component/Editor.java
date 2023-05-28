5251d889985c02915b3d714c7ceefd9e6583e514
taha <rtdonuk@gmail.com>
2023-05-28 14:15:41 +0300

    Formatting shortcut added

--- src/main/java/github/tdonuk/notemanager/gui/component/Editor.java
+++ src/main/java/github/tdonuk/notemanager/gui/component/Editor.java
@@ -1,15 +1,39 @@
 package github.tdonuk.notemanager.gui.component;
 
 import github.tdonuk.notemanager.constant.Application;
+import github.tdonuk.notemanager.util.StringUtils;
 import lombok.SneakyThrows;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeListener;
 
 public class Editor extends RSyntaxTextArea {
 
 	@SneakyThrows
 	public Editor() {
 		super();
+		
+		final Editor editor = this;
+		
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.SHIFT_DOWN_MASK|InputEvent.CTRL_DOWN_MASK), "format");
+		this.getActionMap().put("format", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("formatting " + editor.getSyntaxEditingStyle());
+				if(editor.getText() == null || editor.getText().isBlank()) return;
+				try {
+					if(SyntaxConstants.SYNTAX_STYLE_XML.equals(editor.getSyntaxEditingStyle())) editor.setText(StringUtils.formatXml(editor.getText()));
+				} catch(Exception ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+		}
+	);
 
 		this.setFont(Application.PRIMARY_FONT);
 	}
