801cc8f0090ff68df956a0b28c6166c5fdf36178
Yasin Hizir <yasin.hizir@protonmail.com>
2023-12-04 02:00:15 +0300

    code formatting

--- src/main/java/github/tdonuk/notemanager/domain/State.java
+++ src/main/java/github/tdonuk/notemanager/domain/State.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 @Data
 public class State {
-	private List<FileHist> history;
-	private List<FileHist> currentTabs;
-	private FileHist selectedTab;
-	private Integer caretPosition;
-	private Integer scrollValue;
-	private String leaveDate;
+    private List<FileHist> history;
+    private List<FileHist> currentTabs;
+    private FileHist selectedTab;
+    private Integer caretPosition;
+    private Integer scrollValue;
+    private String leaveDate;
 }
