2c21d6848ca1bab2e3bb2b5f1e77b872225ba1bd
yasin.hizir <yasin.hizir@ekonobi.com>
2023-12-04 01:24:52 +0300

    unused methods removed and code format

--- src/main/java/github/tdonuk/notemanager/util/EnvironmentUtils.java
+++ src/main/java/github/tdonuk/notemanager/util/EnvironmentUtils.java
@@ -7,90 +7,69 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
 import java.io.File;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Slf4j
 public final class EnvironmentUtils {
-	private static final Toolkit toolkit = Toolkit.getDefaultToolkit();
-	
-	public static Dimension screenSize() {
-		return toolkit.getScreenSize();
-	}
-	
-	public static int screenWidth() {
-		return screenSize().width;
-	}
-	
-	public static int screenHeight() {
-		return screenSize().height;
-	}
-	
-	public static Clipboard clipboard() {
-		return toolkit.getSystemClipboard();
-	}
-	
-	public static void copyToClipboard() {
-		// Complete this stub
-	}
-	
-	public static void beep() {
-		toolkit.beep();
-	}
-	
-	public static String osName() {
-		return System.getProperty("os.name");
-	}
-	
-	public static String osVersion() {
-		return System.getProperty("os.version");
-	}
-	
-	public static String osArch() {
-		return System.getProperty("os.arch");
-	}
-	
-	public static String workingDir() {
-		return System.getProperty("user.dir");
-	}
-	
-	public static String userDir() {
-		return System.getProperty("user.home");
-	}
+    private static final Toolkit toolkit = Toolkit.getDefaultToolkit();
 
-	public static String appDataDir() {
-		String path;
-		String osName = osName();
-		if (osName.startsWith("Windows")) {
-			String appDataDir = System.getenv("LOCALAPPDATA");
+    public static Dimension screenSize() {
+        return toolkit.getScreenSize();
+    }
 
-			path = StringUtils.isBlank(appDataDir) ? userDir() : appDataDir;
-		} else if (osName.startsWith("Linux")) {
-			String xdgDataHome = System.getenv("XDG_DATA_HOME");
+    public static int screenWidth() {
+        return screenSize().width;
+    }
 
-			path = StringUtils.isBlank(xdgDataHome) ? userDir() + File.separator + ".local" + File.separator + "share" : xdgDataHome;
-		} else if (osName.startsWith("Mac OS X")) {
+    public static int screenHeight() {
+        return screenSize().height;
+    }
 
-			path = userDir() + File.separator + "Library" + File.separator + "Application Support";
-		} else {
-			throw new CustomException("os not supported: " + osName);
-		}
+    public static void beep() {
+        toolkit.beep();
+    }
 
-		return path + File.separator + "NoteManager";
-	}
+    public static String osName() {
+        return System.getProperty("os.name");
+    }
 
-	public static String stateFileDir() {
-		return appDataDir() + File.separator + "state.json";
-	}
-	
-	public static String tempFilesDir() {
-		return appDataDir() + File.separator + "temp";
-	}
-	
-	public static void shutdown() {
-		log.info("application shutdown");
-		MainWindow.getInstance().dispose();
-		System.exit(-1);
-	}
+    public static String userDir() {
+        return System.getProperty("user.home");
+    }
+
+    public static String appDataDir() {
+        String path;
+        String osName = osName();
+        if (osName.startsWith("Windows")) {
+            String appDataDir = System.getenv("LOCALAPPDATA");
+
+            path = StringUtils.isBlank(appDataDir) ? userDir() : appDataDir;
+        } else if (osName.startsWith("Linux")) {
+            String xdgDataHome = System.getenv("XDG_DATA_HOME");
+
+            path = StringUtils.isBlank(xdgDataHome) ? userDir() + File.separator + ".local" + File.separator + "share" : xdgDataHome;
+        } else if (osName.startsWith("Mac OS X")) {
+
+            path = userDir() + File.separator + "Library" + File.separator + "Application Support";
+        } else {
+            throw new CustomException("os not supported: " + osName);
+        }
+
+        return path + File.separator + "NoteManager";
+    }
+
+    public static String stateFileDir() {
+        return appDataDir() + File.separator + "state.json";
+    }
+
+    public static String tempFilesDir() {
+        return appDataDir() + File.separator + "temp";
+    }
+
+    public static void shutdown() {
+        log.info("application shutdown");
+        MainWindow.getInstance().dispose();
+        System.exit(-1);
+    }
 }
