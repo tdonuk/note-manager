48b5bad07165c4145e4fda7ead304d607022eb8f
Yasin Hizir <yasin.hizir@ekonobi.com>
2023-12-04 01:48:56 +0300

    code formatting

--- src/main/java/github/tdonuk/notemanager/constant/Application.java
+++ src/main/java/github/tdonuk/notemanager/constant/Application.java
@@ -8,9 +8,9 @@ import java.awt.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Application {
-	public static final String VERSION = "1.0.0";
-	public static final String NAME = "Note Manager";
-	public static final ApplicationTheme THEME = ApplicationTheme.LIGHT;
-	public static final Font PRIMARY_FONT = new Font("Courier", Font.PLAIN, 13);
-	public static final Font SECONDARY_FONT = new Font("Courier", Font.PLAIN, 10);
+    public static final String VERSION = "1.0.0";
+    public static final String NAME = "Note Manager";
+    public static final ApplicationTheme THEME = ApplicationTheme.LIGHT;
+    public static final Font PRIMARY_FONT = new Font("Courier", Font.PLAIN, 13);
+    public static final Font SECONDARY_FONT = new Font("Courier", Font.PLAIN, 10);
 }
--- src/main/java/github/tdonuk/notemanager/constant/FileType.java
+++ src/main/java/github/tdonuk/notemanager/constant/FileType.java
@@ -1,25 +1,22 @@
 package github.tdonuk.notemanager.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Arrays;
 
+@RequiredArgsConstructor
+@Getter
 public enum FileType {
-	XML(".xml"), HTML(".html"), TXT(".txt"), JSON(".json"), XSL(".xsl"), XSLT(".xslt");
-	
-	private final String extension;
-	
-	public String getExtension() {
-		return extension;
-	}
-	
-	FileType(String extension) {
-		this.extension = extension;
-	}
-	
-	public static FileType findByExtension(String extension) {
-		try {
-			return Arrays.stream(values()).filter(v -> v.getExtension().equalsIgnoreCase(extension)).findAny().orElse(TXT);
-		} catch(Exception e) {
-			return TXT;
-		}
-	}
+    XML(".xml"), HTML(".html"), TXT(".txt"), JSON(".json"), XSL(".xsl"), XSLT(".xslt");
+
+    private final String extension;
+
+    public static FileType findByExtension(String extension) {
+        try {
+            return Arrays.stream(values()).filter(v -> v.getExtension().equalsIgnoreCase(extension)).findAny().orElse(TXT);
+        } catch (Exception e) {
+            return TXT;
+        }
+    }
 }
