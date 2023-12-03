d40c7f4f0131b18493d024f7217c92e77a9d0dfb
Yasin Hizir <yasin.hizir@gmail.com>
2023-12-04 02:03:23 +0300

    code formatting

--- src/main/java/github/tdonuk/notemanager/exception/CustomException.java
+++ src/main/java/github/tdonuk/notemanager/exception/CustomException.java
@@ -1,11 +1,11 @@
 package github.tdonuk.notemanager.exception;
 
 public class CustomException extends RuntimeException {
-	public CustomException(Exception e) {
-		super(e);
-	}
-	
-	public CustomException(String msg) {
-		super(new Exception(msg));
-	}
+    public CustomException(Exception e) {
+        super(e);
+    }
+
+    public CustomException(String msg) {
+        super(new Exception(msg));
+    }
 }
